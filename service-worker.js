0c24f844a8e3bafb6e806d8c0c90db5b905f9e32
Connor Allen <m.connor.allen@gmail.com>
2022-08-28 20:45:56 -0400

    added service workers

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const FILES_TO_CACHE= [
+    "./public/css/styles.css",
+    "./public/js/idb.js",
+    "./public/js/index.js",
+    "./public/index.html",
+];
+
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME= APP_PREFIX + VERSION;
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+
+})
