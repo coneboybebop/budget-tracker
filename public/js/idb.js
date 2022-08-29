2b20f7e7e2992df72431276b64f57f35fc129535
Connor Allen <m.connor.allen@gmail.com>
2022-08-28 20:08:21 -0400

    added offline persistence

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,4 +27,42 @@ function saveRecord(record) {
     const spendObjectStore = transaction.objectStore('new_spend');
 
     spendObjectStore.add(record);
-}
+};
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_spend'], 'readwrite');
+
+    const spendObjectStore = transaction.objectStore('new_spend');
+
+    const getAll = spendObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw new Error(serverResponse);
+                }
+                //open one more transaction
+                const transaction = db.transaction(['new_spend'], 'readwrite');
+                // access the new_spend object store
+                const spendObjectStore = transaction.objectStore('new_spend');
+                // clear all items in store
+                spendObjectStore.clear();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    };
+};
+
+window.addEventListener('online', uploadTransaction);
